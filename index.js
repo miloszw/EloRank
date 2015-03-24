68d01dc9998cf1ce11823dd734f88ccf4dba0b41
Milosz <miloszw@kth.se>
2015-03-24 19:38:20 +0100

    fix indentation, soft tabs, tab width 2

--- index.js
+++ index.js
@@ -17,35 +17,35 @@ console.log("Listening on 8080...");
 
 // routes
 app.get('/polls/:id(\\d+)?', function(req, res) {
-    if (!req.params.id) {
-        var sql = 'SELECT * FROM polls';
-    } else {
-        var sql = 'SELECT * FROM polls WHERE id=' + req.params.id;
-    }
+  if (!req.params.id) {
+    var sql = 'SELECT * FROM polls';
+  } else {
+    var sql = 'SELECT * FROM polls WHERE id=' + req.params.id;
+  }
 
-    connection.query(sql, function(err, rows, fields) {
-      if (err) throw err;
+  connection.query(sql, function(err, rows, fields) {
+    if (err) throw err;
 
-      // get alternatives for each poll
-        var promises = _.range(0,rows.length)
-        .map(function(i) {
-            return new RSVP.Promise(function(resolve, reject) {
-                connection.query('SELECT * FROM alternatives WHERE polls_id=?',
-                    rows[i].id, function(err, rowsAlt, fields) {
-                        if (err) throw err;
-                        rows[i].alternatives = rowsAlt;
-                        console.log("Added alts to row id" + i);
-                        resolve();
-                    });    
-            })
+    // get alternatives for each poll
+    var promises = _.range(0,rows.length)
+    .map(function(i) {
+      return new RSVP.Promise(function(resolve, reject) {
+        connection.query('SELECT * FROM alternatives WHERE polls_id=?',
+        rows[i].id, function(err, rowsAlt, fields) {
+          if (err) throw err;
+          rows[i].alternatives = rowsAlt;
+          console.log("Added alts to row id" + i);
+          resolve();
         });
-
-        RSVP.all(promises).then(function() {
-          console.log("Sent response!");
-          res.send(rows);
-          res.end();
-        });
+      })
     });
+
+    RSVP.all(promises).then(function() {
+      console.log("Sent response!");
+      res.send(rows);
+      res.end();
+    });
+  });
 
 });
 
@@ -53,7 +53,7 @@ app.get('/polls/:id(\\d+)?', function(req, res) {
 
 // on ctrl-c
 process.on('SIGINT', function() {
-    connection.end();
-    console.log("\nExiting...");
-    process.exit();
+  connection.end();
+  console.log("\nExiting...");
+  process.exit();
 });
