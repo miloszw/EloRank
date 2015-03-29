105b2ab6c775aebcce9d2f76ef23f77522d56144
Milosz <miloszw@kth.se>
2015-03-29 23:48:50 +0200

    add proper Elo ranking

--- elo.js
+++ elo.js
@@ -1,7 +1,21 @@
-module.exports = {
-  k: 0.32,
+var elo = {
+  k: 32,
+  initialScore: 400,
   calcScore: function(result, scoreA, scoreB) {
-    // dummy equations, not true elo
-    return [scoreA * (1+this.k), scoreB * (1-this.k)];
+    // result: 0 if A wins, 1 if B wins
+    // returns: [new score for A, new score for B]
+    return [rank(result ^ 1, scoreA, expectedScore(scoreA, scoreB)),
+    rank(result ^ 0, scoreB, expectedScore(scoreB, scoreA))];
   }
 }
+
+var expectedScore = function(ratingA, ratingB) {
+  // returns expected score for A
+  return 1/(1 + Math.pow(10, (ratingB - ratingA)/elo.initialScore));
+}
+
+var rank = function(outcome, rank, expectedScore) {
+  return rank + elo.k * (outcome - expectedScore);
+}
+
+module.exports = elo
