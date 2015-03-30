35c91405265b2f9b5519a262caa823a5f95fe2ad
miloszw <miloszw@kth.se>
2015-03-30 16:13:38 +0200

    clean up the db init file

--- db.sql
+++ db.sql
@@ -2,8 +2,13 @@
 CREATE DATABASE /*!32312 IF NOT EXISTS*/ `elorank` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
 USE `elorank`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
+CREATE TABLE `polls` (
+  `name` varchar(45) DEFAULT NULL,
+  `id` int(8) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
 CREATE TABLE `alternatives` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `url` varchar(120) DEFAULT NULL,
@@ -15,9 +20,7 @@ CREATE TABLE `alternatives` (
   KEY `polls_id` (`polls_id`),
   CONSTRAINT `alternatives_ibfk_1` FOREIGN KEY (`polls_id`) REFERENCES `polls` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `challenges` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `poll_id` int(11) NOT NULL,
@@ -31,12 +34,3 @@ CREATE TABLE `challenges` (
   CONSTRAINT `challenges_ibfk_2` FOREIGN KEY (`alt2_id`) REFERENCES `alternatives` (`id`) ON DELETE CASCADE,
   CONSTRAINT `challenges_ibfk_3` FOREIGN KEY (`poll_id`) REFERENCES `polls` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=502 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `polls` (
-  `name` varchar(45) DEFAULT NULL,
-  `id` int(8) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
